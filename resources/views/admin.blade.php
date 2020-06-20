ec98a6086fad57b22f7ab54c9b3b2931636daa00
Виталий Качурин <citrix@mail.ru>
2020-06-20 13:30:46 +0300

    +

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,23 +5,46 @@
    
 
     <table width="100%" border="1" cellpadding="4" cellspacing="0">
-
+    <tr>
+            <th><p class='text-center'>Id</p></th><th><p class='text-center'>Дата</p></th><th><p class='text-center'>Имя</p></th><th><p class='text-center'>email</p></th><th><p class='text-center'>Комментарий</p></th><th><p class='text-center'>Действие</p></th>
+    </tr>
 
         
         @foreach ($comments as $comment)    
-    
-      {{ $comment->date }}  
+    <tr></tr>
+     <td>{{ $comment->id }}  </td> 
+     <td>{{ $comment->date }}  </td> 
+     <td>{{ $comment->name }}  </td> 
+     <td>{{ $comment->email }}  </td> 
+     <td>{{ $comment->comment }}  </td> 
+     <td><button type="button" class="btn btn-success">Разрешить</button>     
+      <button type="button" class="btn btn-danger">Удалить</button> </td>      
+    </tr>
         @endforeach 
+</table>
+<br>
+<br>
+<br>
+<br>
 
 
     <table width="100%" border="1" cellpadding="4" cellspacing="0">
+    <tr>
+           <th><p class='text-center'>Фото</p></th> <th><p class='text-center'>Id</p></th><th><p class='text-center'>Название конференции</p></th><th><p class='text-center'>Дата</p></th><th><p class='text-center'>Место проведения</p></th><th><p class='text-center'>Действие</p></th>
+    </tr>    
+    @foreach ($comments as $comment)    
+    <tr></tr>
+     <td>{{ $comment->photo_link }}  </td>     
+     <td>{{ $comment->id }}  </td> 
+     <td>{{ $comment->conference_name }}  </td> 
+     <td>{{ $comment->date }}  </td> 
+     <td>{{ $comment->venue }}  </td> 
 
 
-        
-    @foreach ($conference_list as $list)    
-
-  {{ $list->date }}  
-
-  @endforeach 
+     <td><button type="button" class="btn btn-success">Разрешить</button>      
+     <button type="button" class="btn btn-danger">Удалить</button> </td>      
+    </tr>
+        @endforeach 
+</table>
 </body>
 
